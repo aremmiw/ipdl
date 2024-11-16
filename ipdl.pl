944cb8ef945fd1817844ccfad31a1df2d82288ec
aremmiw <171315007+aremmiw@users.noreply.github.com>
2024-11-16 19:16:57 +1300

    Partly working now. switched spaces to tabs

--- ipdl.pl
+++ ipdl.pl
@@ -2,15 +2,15 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Regexp::Common qw(URI);
+use Regexp::Common qw(URI net);
 use LWP::Simple;
 
 my $output = "";
 my $help;
 
 GetOptions ("output=s" => \$output,
-            "help"     => \$help,
-           ) || die 'Error: invalid options';
+	    "help"     => \$help,
+	    ) || die 'Error: invalid options';
 
 print_help() unless (!$help);
 
@@ -20,39 +20,52 @@ my @files;
 my $linkcounter = 0;
 
 foreach (@ARGV) {
-    unless (check_url($_) || check_file_read($_)) {
-        die "Invalid file/url: $_";
-    }
+	unless (check_url($_) || check_file_read($_)) {
+		die "Invalid file/url: $_";
+	}
 }
 
-foreach (@files) {
-        print("$_\n")
+open(my $outfh, '>', $output) || die $!;
+
+my %seenips;
+
+foreach my $file (@files) {
+	open(my $ifileh, '<', $file) || die $!;
+	while (<$ifileh>) {
+		chomp;
+		if ((/$RE{net}{IPv4}{-keep}/ || /$RE{net}{IPv6}{-style => 'HeX'}{-keep}/) && !$seenips{$_}++) {
+			print $outfh "$1\n"
+		}
+	}
+	close $ifileh;
 }
+
+close $outfh || warn "Closing output failed!";
 
 sub check_url {
-    shift;
-    if ($_ =~ $RE{URI}{HTTP}{-scheme => qr<https?>}) {
-        my $temppath = "/tmp/ipdl$linkcounter.txt";
-        my $dl = getstore($_, $temppath);
-        if (defined($dl) && $dl == 200) {
-            push(@files, $temppath);
-            $linkcounter++;
-            return 1;
-        }
-        return 0;
-    }
+	shift;
+	if (/$RE{URI}{HTTP}{-scheme => qr<https?>}/) {
+		my $temppath = "/tmp/ipdl$linkcounter.txt";
+		my $dl = getstore($_, $temppath);
+		if (defined($dl) && $dl == 200) {
+			push(@files, $temppath);
+			$linkcounter++;
+			return 1;
+		}
+		return 0;
+	}
 }
 
 sub check_file_read {
-    shift;
-    if (-e $_ && -r $_) {
-        push(@files, $_);
-        return 1;
-    }
-    return 0;
+	shift;
+	if (-e $_ && -r $_) {
+		push(@files, $_);
+		return 1;
+	}
+	return 0;
 }
 
 sub print_help {
-    print 'This is the help...';
-    exit;
+	print 'This is the help...';
+	exit;
 }
