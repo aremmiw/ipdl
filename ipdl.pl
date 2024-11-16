cf277d4f3f9faf0d9b3c4bb4f7c11efa25456b98
aremmiw <171315007+aremmiw@users.noreply.github.com>
2024-11-16 17:47:01 +1300

    initial commit

---
+++ ipdl.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Regexp::Common qw(URI);
+use LWP::Simple;
+
+my $output = "";
+my $help;
+
+GetOptions ("output=s" => \$output,
+            "help"     => \$help,
+           ) || die 'Error: invalid options';
+
+print_help() unless (!$help);
+
+die 'Error: No output given!' unless (length $output); #TODO: check that path is writable
+
+my @files;
+my $linkcounter = 0;
+
+foreach (@ARGV) {
+    unless (check_url($_) || check_file_read($_)) {
+        die "Invalid file/url: $_";
+    }
+}
+
+foreach (@files) {
+        print("$_\n")
+}
+
+sub check_url {
+    shift;
+    if ($_ =~ $RE{URI}{HTTP}{-scheme => qr<https?>}) {
+        my $temppath = "/tmp/ipdl$linkcounter.txt";
+        my $dl = getstore($_, $temppath);
+        if (defined($dl) && $dl == 200) {
+            push(@files, $temppath);
+            $linkcounter++;
+            return 1;
+        }
+        return 0;
+    }
+}
+
+sub check_file_read {
+    shift;
+    if (-e $_ && -r $_) {
+        push(@files, $_);
+        return 1;
+    }
+    return 0;
+}
+
+sub print_help {
+    print 'This is the help...';
+    exit;
+}
